0fa1c1b1f45221d9b7dcbb9796cc573cfdbf80db
OgagaAkpowenre1 <136248640+OgagaAkpowenre1@users.noreply.github.com>
2024-02-25 02:09:38 +0100

    On branch master  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/navbar/index.jsx 	new file:   src/components/recipe-item/recipe-item.jsx 	new file:   src/context/index.jsx 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/pages/details/index.jsx 	new file:   src/pages/favorites/index.jsx 	new file:   src/pages/home/index.jsx 	new file:   tailwind.config.js

---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,21 @@
+import { GlobalContext } from "../../context";
+import { useContext } from "react";
+import RecipeItem from "../../components/recipe-item/recipe-item";
+
+export default function Favorites() {
+	const { favoritesList } = useContext(GlobalContext);
+
+	return (
+		<div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+			{favoritesList && favoritesList.length > 0 ? (
+				favoritesList.map((item) => <RecipeItem item={item} />)
+			) : (
+				<div>
+					<p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+						Nothing added. Please add some recipes.
+					</p>
+				</div>
+			)}
+		</div>
+	);
+}
